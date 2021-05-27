507cc72c6a60a1c7fb2aa7438364962b75290d18
drrobb04 <drrobb04@gmail.com>
2021-05-27 10:51:43 -0600

    adding initial schema set up

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE meters (
+	Well Name TEXT,
+	Meter ID
+);
+
+CREATE TABLE scada (
+	Well Name TEXT,
+	Date,
+
+
+);
+
+CREATE TABLE prams (
+
+
+
+);
+
+CREATE TABLE gas_prices(
+	
+
+
+);
