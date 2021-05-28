27c7cc7eee7c3cc061d6bb654ba05d4af4442d47
drrobb04 <drrobb04@gmail.com>
2021-05-27 19:24:38 -0600

    initial schema set up

--- schema.sql
+++ schema.sql
@@ -1,23 +1,30 @@
+-- Create meters table
 CREATE TABLE meters (
-	Well Name TEXT,
-	Meter ID
+	Well Name VARCHAR not null,
+	Meter ID VARCHAR not null,
+	PRIMARY KEY ()
 );
 
+-- Create scada table
 CREATE TABLE scada (
-	Well Name TEXT,
-	Date,
-
-
+	Well Name TEXT not null,
+	Date DATE not null,
+	Yesterdays Gas Production (Mcf) DECIMAL not null,
+	Meter ID VARCHAR not null,
+	PRIMARY KEY ()
 );
 
+-- Create prams table
 CREATE TABLE prams (
-
-
-
+	Date DATE not null,
+	Well Name VARCHAR not null,
+	Total Gas Production (Mcf) DECIMAL not null,
+	PRIMARY KEY ()
 );
 
+-- Create gas_prices table
 CREATE TABLE gas_prices(
-	
-
-
+	Week Of DATE not null,
+	Avg US Gas Price ($/Gal) DECIMAL not null,
+	PRIMARY KEY
 );
