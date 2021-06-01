ccad9cbb584601ee68a729396b69fc7b7b2e5491
leahgraced <77127752+leahgraced@users.noreply.github.com>
2021-06-01 17:15:13 -0600

    pushing pandas and schema changes

--- schema.sql
+++ schema.sql
@@ -1,36 +1,24 @@
 CREATE TABLE "Meter_Combo" (
     "Well_Name" VARCHAR   NOT NULL,
-    "Meter_ID" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Meter_Combo" PRIMARY KEY (
-        "Meter_ID"
-     )
+    "Meter_ID" VARCHAR   NOT NULL
 );
 
 CREATE TABLE "scada_data" (
     "Well_Name" VARCHAR   NOT NULL,
     "Date" DATE   NOT NULL,
     "Yesterdays_Gas_Production_(Mcf)" DECIMAL   NOT NULL,
-    "Meter_ID" VARCHAR  NOT NULL,
-	CONSTRAINT "pk_scada_data" PRIMARY KEY (
-        "Meter_ID"
-		)
+    "Meter_ID" VARCHAR  NOT NULL
 );
 
 CREATE TABLE "prams_data" (
     "Date" DATE   NOT NULL,
     "Well_Name" VARCHAR   NOT NULL,
-    "Total_Gas_Production_(Mcf)" DECIMAL   NOT NULL,
-    CONSTRAINT "pk_prams_data" PRIMARY KEY (
-        "Well_Name"
-     )
+    "Total_Gas_Production_(Mcf)" DECIMAL   NOT NULL
 );
 
 CREATE TABLE "gas_prices" (
     "Week_Of" DATE   NOT NULL,
-    "Avg_US_Gas_Price_($/Gal)" DECIMAL   NOT NULL,
-    CONSTRAINT "pk_gas_prices" PRIMARY KEY (
-        "Week_Of"
-     )
+    "Avg_US_Gas_Price_($/Gal)" DECIMAL   NOT NULL
 );
 
 ALTER TABLE "scada_data" ADD CONSTRAINT "fk_scada_data_Well_Name" FOREIGN KEY("Well_Name")
